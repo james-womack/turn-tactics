607de4a6b7266cda394c83b22876449aab979a7d
James Womack <womackrjames@gmail.com>
2017-10-03 20:31:13 -0700

    Started on mapping all key stuff

--- Src/Common.h
+++ Src/Common.h
@@ -1,5 +1,8 @@
 #pragma once
-#include <array>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
 
 // Type defs for raw characters
 using UChar = unsigned char;
@@ -25,10 +28,53 @@ using Float64 = double;
 
 namespace lse
 {
-  enum class Renderer : UChar
+  using String = std::string;
+
+  // Maps stl containers to alias's incase we implement own data
+  // structures later
+  namespace containers
   {
-    Undefined,
-    SDL,
-    OpenGL // Not implemented yet...
-  };
+    template<class TKey, class TValue>
+    using Map = std::map<TKey, TValue>;
+
+    template<class TKey, class TValue>
+    using UnorderedMap = std::unordered_map<TKey, TValue>;
+
+    template<class TValue>
+    using Vector = std::vector<TValue>;
+    
+
+  }
+
+  // Converts enum class to underlying type
+  template<typename E>
+  constexpr auto to_integral(E e) -> typename std::underlying_type<E>::type {
+    return static_cast<typename std::underlying_type<E>::type>(e);
+  }
+
+  // Converts a string to upper case inplace (replacing old string)
+  inline void to_upper_inplace(String& str)
+  {
+    std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+  }
+
+  // Converts a string to upper case creating a copy and returning it
+  inline String to_upper(String str)
+  {
+    std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+    return std::move(str);
+  }
+
+  // Converts a string to lower case inplace (replacing old string)
+  inline void to_lower_inplace(String& str)
+  {
+    std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+  }
+
+  // Converts a string to lower case creating a copy and returning it
+  inline String to_lower(String str)
+  {
+    std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+    return std::move(str);
+  }
 }
--- Src/Render/Texture.h
+++ Src/Render/Texture.h
@@ -1,10 +1,52 @@
+/******************************************************************************
+File: Texture.h
+Created: 10/1/2017 11:49:28 PM
+Copyright (c) 2017 Turn Tactics
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Purpose: Contains all of the methods for manipulating textures for SDL
+
+Author: James Womack
+
+********************************************************************************/
 #pragma once
-#ifndef _TEXTURE_H_
-#define _TEXTURE_H_
+
+#include "../Common.h"
 
 #include <SDL.h>
 #include <SDL_image.h>
 
+namespace lse
+{
+  enum class TextureFileType : Int8
+  {
+    Undefined,
+    BMP,
+    JPG,
+    PNG,
+    TGA,
+    TIF
+  };
 
-
-#endif
+  class Texture
+  {
+    
+  };
+}
